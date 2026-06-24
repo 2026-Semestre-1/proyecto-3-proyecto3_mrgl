ae81ed06bebf342cfd5838d8dce018471330bc8b
JoshuaMRGL <j.madrigal.4@estudiantec.cr>
2026-06-23 21:11:00 -0600

    Add files via upload
    Joshua Madrigal
    
    Carné:2026085545

---
+++ Código/comit3.py
@@ -0,0 +1,150 @@
+import tkinter as tk
+from tkinter import *
+
+
+""""
+Nombre:
+entrada:
+salida:
+restricciones:
+"""""
+
+
+class País:
+    def __init__(self,codigofifa,nombres,continentes,ranking_fifa):
+        self.codigo_fifa=codigofifa
+        self.nombre=nombres
+        self.continente=continentes
+        self.ranking_fifas=ranking_fifa
+    def mostrar_datos(self):
+        codigo=self.codigo_fifa
+        nombre=self.nombre
+        continente=self.continente
+        ranking=self.ranking_fifas
+        print(f"""              
+              Código del país:{codigo}
+               Nombre del país:{nombre}
+               Continente:{continente}
+               Ranking de la FIFA:{ranking}
+""")
+    def actualizar_datos(self):
+        joshua=joshua#Falta Terminarlo, hay que hacer los constructores y las clases primero
+class Persona:
+    def __init__(self,nombre,apellido,fecha_nacimiento,nacionalidad):
+        self.nombre=nombre
+        self.apellido=apellido
+        self.fecha_nacimiento=fecha_nacimiento
+        self.nacionalidad=nacionalidad
+        if not isinstance(nombre,str):
+            return "Error:el nombre tiene que ser en formato de letras"
+        elif not isinstance(apellido,str):
+            return "Error:El apellido tiene que ser en formato de letras"
+        elif not isinstance(fecha_nacimiento,str):
+            return "Error:La fecha de nacimiento debe de ser en formato de letras"
+        elif not isinstance(nacionalidad,str):
+            return "Error:La nacionalidad debe de ser de formato de letras"
+        
+    def mostrar(self):
+        print(f"""
+Nombre de la persona:{self.nombre}
+Apellido de la persona:{self.apellido}
+nacionalidad:{self.nacionalidad}
+Fecha de nacimiento:{self.fecha_nacimiento}
+""")
+class Entrenador(Persona):
+    def __init__(self, nombre,apellido,fecha_nacimiento,nacionalidad,licencia,experiencia_años,sistemadejuego):
+        self.nombre=nombre
+        self.apellido=apellido
+        self.fecha_nacimiento=fecha_nacimiento
+        self.nacionalidad=nacionalidad
+        self.licencia=licencia
+        self.experiencia_años=experiencia_años
+        self.sistemadejuego=sistemadejuego
+        if not isinstance(nombre,str):
+            return "Error:el nombre tiene que ser en formato de letras"
+        elif not isinstance(apellido,str):
+            return "Error:El apellido tiene que ser en formato de letras"
+        elif not isinstance(fecha_nacimiento,str):
+            return "Error:La fecha de nacimiento debe de ser en formato de letras"
+        elif not isinstance(nacionalidad,str):
+            return "Error:La nacionalidad debe de ser de formato de letras"
+
+        
+        if not isinstance(licencia,str):
+            return "Error:debe de ser de formato de letras"
+        elif not isinstance(experiencia_años,int):
+            return "Error:debe de ser en formato numerico"
+        elif not isinstance(sistemadejuego,str):
+            return "Error:debe de ser en formato de letras"
+    def mostrar_datos(self):
+        print(f"""
+            Nombre del entrenador:{self.nombre}
+            licencia:{self.licencia}
+            Años de experencia:{self.experiencia_años}
+            Sistema de juego:{self.sistemadejuego}
+""")
+    def actualizar_datos(self):
+        print("")  
+
+class jugador(Persona) :
+    def __init__(self,nombre,apellido,fecha_nacimiento,nacionalidad,dorsal,posicion,total_tarjetas_amarillas,total_tarjetas_rojas,goles,asistencias,puntaje_individual):
+        self.nombre=nombre
+        self.apellido=apellido
+        self.fecha_nacimiento=fecha_nacimiento
+        self.nacionalidad=nacionalidad
+        self.dorsal=dorsal
+        self.posicion=posicion
+        self.total_tarjetas_amarillas=total_tarjetas_amarillas
+        self.total_tarjetas_rojas=total_tarjetas_rojas
+        self.goles=0
+        self.asistencias=0
+        self.puntaje_individual=0#TXT
+    def mostrar_datos(self):
+        print(f"""
+                Nombre del jugador:{self.nombre}
+                Apellido del jugador:{self.apellido}
+                Fecha de nacimiento:{self.fecha_nacimiento}
+                Nacionalidad:{self.nacionalidad}
+                Dorsal:{self.dorsal}
+                Posición del jugador:{self.posicion}
+                Estadísticas: 
+                            goles:{self.goles}
+                            Asistencias:{self.asistencias}
+                puntaje individual:{self.puntaje_individual}
+""")
+    def actualizar_datos(self):
+        #txt
+        print()
+    def registrar_gol(self):
+        self.goles+=1
+    def registrar_asistencia(self):
+        self.asistencias+=1
+    def registrar_tarjetas(self,tipo):
+        #investigar como hacerlo
+        print()
+
+
+
+ventana=tk.Tk()
+ventana.title("COPA MUNDIAL")
+ventana.geometry("800x800")
+fondo=PhotoImage(file="logo2026.png")
+ventana.configure(bg="")
+fondo_label=Label(ventana,image=fondo)
+fondo_label.place(x=0,y=0,relwidth=1,relheight=1)
+fondo_label=fondo
+
+marco_main=tk.Frame(ventana,bg="gold",bd=2,relief="ridge")
+marco_main.place(relx=0.5,rely=0.5,anchor="center",width=500,height=450)
+####################################################################################################################################################################################################
+boton_adminPais=tk.Button(marco_main,text="administrar países y selecciones",font=("Segoe Ui",12,"bold"),bg="#076529",fg="white",activebackground="#3b9d22",relief="flat",cursor="hand2")
+boton_adminPais.pack(pady=25,padx=25,fill="x")
+boton_adminEntre=tk.Button(marco_main,text="Administrar entrenadres y jugadores",font=("Segoe Ui",12,"bold"),bg="#0C2268",fg="White",activebackground="#0C2268",relief="flat",cursor="hand2")
+boton_adminEntre.pack(pady=25,padx=25,fill="x")
+boton_configMun=tk.Button(marco_main,text="Configurar Mundial",font=("Segoe Ui",12,"bold"),bg="#076529",fg="white",activebackground="#3b9d22",relief="flat",cursor="hand2")
+boton_configMun.pack(padx=25,pady=25,fill="x")
+boton_jugarM=tk.Button(marco_main,text="Jugar Mundial",font=("Segoe Ui",12,"bold"),bg="#0C2268",fg="White",activebackground="#0C2268",relief="flat",cursor="hand2")
+boton_jugarM.pack(padx=25,pady=25,fill="x")
+boton_Estadistica=tk.Button(marco_main,text="Ver Estadística",font=("Segoe Ui",12,"bold"),bg="#076529",fg="white",activebackground="#3b9d22",relief="flat",cursor="hand2")
+boton_Estadistica.pack(padx=25,pady=25,fill="x")
+ventana.mainloop()
